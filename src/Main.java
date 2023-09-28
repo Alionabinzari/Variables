dd5f0e55383fab85e30b273d3e2d5ea6523c13bb
aliona.binzari <alionab79@gmail.com>
2023-09-28 11:47:54 +0300

    Tema pentru acasa, Variables, ex.2

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        long cwp =7811080815L;
+        long cpty=55235358L;
+        char flia='A';
+        boolean jia=true;
+        float pi=3.14159F;
+
+        System.out.println("Curent World Populationation - "+cwp);
+        System.out.println("Cars Produced This Year - "+cpty);
+        System.out.println("First Letter In Alphabet - "+flia);
+        System.out.println("Java Is Amazing - "+jia);
+        System.out.println("Pi - "+pi);
+
+    }
+}
